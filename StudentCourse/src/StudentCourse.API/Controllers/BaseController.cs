bba99cd6caea98bad9bbab95f4f118a3f818914e
Fullstack Hub <35650464+fullstackhub-io@users.noreply.github.com>
2021-05-16 01:25:09 -0400

    Added student course controller.

---
+++ StudentCourse/src/StudentCourse.API/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentCourse.API.Controllers
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private IMediator mediator;
+
+        /// <summary>
+        /// Gets the Mediator.
+        /// </summary>
+        protected IMediator Mediator => this.mediator ??= this.HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
---
+++ StudentCourse/src/StudentCourse.API/Controllers/StudentControllerController.cs
@@ -0,0 +1,44 @@
+﻿using Course.Application.User.Commands;
+using Microsoft.AspNetCore.Mvc;
+using StudentCourse.Application.User.Commands;
+using System.Threading.Tasks;
+
+namespace StudentCourse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentCourseController : BaseController
+    {
+
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<CourseVM>> Get(int id)
+        //{
+        //    return await this.Mediator.Send(new GetSingleCourseQuery { CourseID = (int)id });
+        //}
+
+        //[HttpGet]
+        //public async Task<ActionResult<CourseVM>> Get()
+        //{
+        //    return await this.Mediator.Send(new GetAllCourseQuery());
+        //}
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(AddStudentCourseCommand command)
+        {
+            return await this.Mediator.Send(command);
+        }
+
+        //[HttpPut]
+        //public async Task<ActionResult<bool>> Put(UpdateCourseCommand command)
+        //{
+        //    return await this.Mediator.Send(command);
+        //}
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            return await this.Mediator.Send(new DeleteStudentCourseCommand { StudentCourseID = id });
+        }
+
+    }
+}
